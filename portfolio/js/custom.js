3ee5f2af2e36e8ca81046be2b95102589f58a717
Alfia Malakshinova <itsalfia@gmail.com>
2017-06-22 00:12:15 +0600

    Commit

---
+++ portfolio/js/custom.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//
+//   $(".seemore").on("mouseover", function(){
+//     $(this).attr("src", "images/12.png");
+//     // $(".seemore").attr("src", "images/11.png");
+//   });
+//   $(".seemore").on("mouseout", function(){
+//     $(this).attr("src", "images/11.png");
+//   });
+//
+//
+//
+// });
