3c0385fe821468055bef4b8515f45951020c04d7
Alfia Malakshinova <itsalfia@gmail.com>
2017-07-02 02:45:10 +0600

    commit

--- zhasotan/js/custom.js
+++ zhasotan/js/custom.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  $("h1").on("click", function(){
-    $("h1").hide();
+  $("li").on("click", function(){
+    $("li").removeClass("active");
+    $(this).addClass("active");
   });
+  // $('#carouselFade').carousel();
+  $('#buttonsearch').click(function(){
+				$('#formsearch').slideToggle( "fast",function(){
+					 $( '#content' ).toggleClass( "moremargin" );
+				} );
+				$('#searchbox').focus()
+				$('.openclosesearch').toggle();
+		});
+
+
+
 });
